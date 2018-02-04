07ca67501d70a1535c71a24caa553346c9e9a967
xiaobin.zhang <xiaobin.zhang@baichanghui.com>
2018-02-04 16:48:16 +0800

    修改package.json

---
+++ src/home/home.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="home">
+		{{msg}}
+	</div>
+</template>
+<script>
+	export default{
+		data: function(){
+			return {
+				msg: '这是home文件',
+			}
+		}
+	}
+</script>
+<style>
+.home {
+	font-size: 30px;
+	color: red;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/home/home.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+	{
+      path: '/home.html',
+      name: 'Home',
+      component: Home
+    }    
   ]
 })
