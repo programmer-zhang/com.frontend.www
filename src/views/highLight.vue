1c1c09ad541227e3b5afcde03a6684a638984c77
programmer-zhang <chinajnzhang@hotmail.com>
2018-08-07 23:11:09 +0800

    提交更新

--- src/views/highLight.vue
+++ src/views/highLight.vue
@@ -1,6 +1,7 @@
 <template>
-	<div class="high-light">
-		{{testData}}
+	<div class="high-light" id="highLight">
+		<div>{{testData}}</div>
+		<button @click="searchHighLight('highLight','旅')">点击查看高亮</button>
 	</div>
 </template>
 <script>
@@ -11,12 +12,55 @@
 			}
 		},
 		methods: {
-
+			searchHighLight:function(idVal, keyword) {
+				var pucl = document.getElementById(idVal);
+	            if ("" == keyword) return;
+	            var temp = pucl.innerHTML;
+	            var htmlReg = new RegExp("\<.*?\>", "i");
+	            var arrA = new Array();
+	            //替换HTML标签
+	            for (var i = 0; true; i++) {
+	                var m = htmlReg.exec(temp);
+	                if (m) {
+	                    arrA[i] = m;
+	                }
+	                else {
+	                    break;
+	                }
+	                temp = temp.replace(m, "{[(" + i + ")]}");
+	            }
+	            debugger
+	            words = decodeURIComponent(keyword.replace(/\+/g, ' ')).split(/\s+/);
+	            //替换关键字
+	            for (w = 0; w < words.length; w++) {
+	                var r = new RegExp("(" + words[w].replace(/[(){}.+*?^$|\\\[\]]/g, "\\$&") + ")", "ig");
+	                temp = temp.replace(r, "<b style='color:Red;'>$1</b>");
+	            }
+	            //恢复HTML标签
+	            for (var i = 0; i < arrA.length; i++) {
+	                temp = temp.replace("{[(" + i + ")]}", arrA[i]);
+	            }
+	            pucl.innerHTML = temp;
+	            //写入iframe
+	            /*
+	            var iframe = document.getElementById('iframe1');
+	            iframe.contentDocument.body.innerHTML = temp;
+	            */
+			}
 		}
 	}
 </script>
 <style lang="scss">
 .high-light {
-	color: red;
+	color: #000;
+	button {
+		display: block;
+		height: 50px;
+		line-height: 50px;
+		padding: 0 10px;
+		margin: 0 auto;
+		border-radius: 4px;
+		background-color: #dfdfdf;
+	}
 }
 </style>
