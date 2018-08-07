cf47c2ccc0062297b1928d8affab2981ff7da05a
xiaobin.zhang <xiaobin.zhang@baichanghui.com>
2018-08-07 22:52:53 +0800

    提交模糊查询高亮

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Home from '@/home/home.vue'
+import Home from '@/views/home.vue'
+import HighLight from '@/views/highLight.vue'
+import Pure from '@/test/pure.vue'
+import pureModel from '@/test/pure-model.vue'
+
 
 Vue.use(Router)
 
@@ -16,6 +20,21 @@ export default new Router({
       path: '/home.html',
       name: 'Home',
       component: Home
-    }    
+    },
+    {
+      path: '/highLight.html',
+      name: 'HighLight',
+      component: HighLight
+    },    
+    {
+        path: '/pure.html',
+        name: 'Pure',
+        component: Pure
+    },
+    {
+        path: '/pure-model.html',
+        name: 'pureModel',
+        component: pureModel
+    }        
   ]
 })
