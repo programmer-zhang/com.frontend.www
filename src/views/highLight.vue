c1446a62b3d2ccc3bbd4b0fde068035ba0733562
xiaobin.zhang <xiaobin.zhang@baichanghui.com>
2018-08-07 22:53:04 +0800

    提交模糊查询高亮

---
+++ src/views/highLight.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="high-light">
+		{{testData}}
+	</div>
+</template>
+<script>
+	export default {
+		data: function(){
+			return {
+				testData: '旅游,旅行,飞机票,火车票,五星级,酒店,住宿,小时房,民宿,客栈,公寓,便宜,打折,查询,预定,预订,助手,app,软件,平台,网站,携程,去哪儿,同程,艺龙,美团,小猪短租,驴妈妈,比特币,以太坊'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+<style lang="scss">
+.high-light {
+	color: red;
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="home">
+		{{msg}}
+		<div v-for="(item, index) in days">{{index}}</div>
+	</div>
+</template>
+<script>
+	export default{
+		data: function(){
+			return {
+				msg: '这是home文件',
+				days: 6
+			}
+		}
+	}
+</script>
+<style>
+.home {
+	font-size: 30px;
+	color: red;
+}
+</style>
