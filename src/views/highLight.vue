c8f7b9b97c57637dd6cad62147a4fff06c5b28fc
programmer-zhang <chinajnzhang@hotmail.com>
2018-08-08 00:12:16 +0800

    提交更新

--- src/views/highLight.vue
+++ src/views/highLight.vue
@@ -1,26 +1,27 @@
 <template>
-	<div class="high-light" id="highLight">
-		<div>{{testData}}</div>
-		<button @click="searchHighLight('highLight','旅')">点击查看高亮</button>
+	<div class="high-light">
+		<div id="highLight">{{testData}}</div>
+		<button @click="searchHighLight('highLight','a')">点击查看高亮</button>
+		<button @click="searchHighLight2('highLight','sb')">点击查看高亮2</button>
 	</div>
 </template>
 <script>
 	export default {
 		data: function(){
 			return {
-				testData: '旅游,旅行,飞机票,火车票,五星级,酒店,住宿,小时房,民宿,客栈,公寓,便宜,打折,查询,预定,预订,助手,app,软件,平台,网站,携程,去哪儿,同程,艺龙,美团,小猪短租,驴妈妈,比特币,以太坊',
+				testData: '旅游,旅行,飞机票,火车票,五星级,酒店,住宿,小时房,民宿,客栈,公寓,便宜,打折,查询,预定,预订,助手,app,软件,平台,网站,携程,去哪儿,同程,艺龙,美团,小猪短租,驴妈妈,比特币,以太坊,a,ba,asdf,sbhjfab,sbajaquw,asbckbi',
 			}
 		},
 		methods: {
 			searchHighLight:function(idVal, keyword) {
-				var pucl = document.getElementById(idVal);
+				let pucl = document.getElementById(idVal);
 	            if ("" == keyword) return;
-	            var temp = pucl.innerHTML;
-	            var htmlReg = new RegExp("\<.*?\>", "i");
-	            var arrA = new Array();
+	            let temp = pucl.innerHTML;
+	            let htmlReg = new RegExp("\<.*?\>", "i");
+	            let arrA = new Array();
 	            //替换HTML标签
-	            for (var i = 0; true; i++) {
-	                var m = htmlReg.exec(temp);
+	            for (let i = 0; true; i++) {
+	                let m = htmlReg.exec(temp);
 	                if (m) {
 	                    arrA[i] = m;
 	                }
@@ -29,23 +30,26 @@
 	                }
 	                temp = temp.replace(m, "{[(" + i + ")]}");
 	            }
-	            debugger
-	            words = decodeURIComponent(keyword.replace(/\+/g, ' ')).split(/\s+/);
+	            let words = decodeURIComponent(keyword.replace(/\+/g, ' ')).split(/\s+/);
 	            //替换关键字
-	            for (w = 0; w < words.length; w++) {
-	                var r = new RegExp("(" + words[w].replace(/[(){}.+*?^$|\\\[\]]/g, "\\$&") + ")", "ig");
+	            for (let w = 0; w < words.length; w++) {
+	                let r = new RegExp("(" + words[w].replace(/[(){}.+*?^$|\\\[\]]/g, "\\$&") + ")", "ig");
 	                temp = temp.replace(r, "<b style='color:Red;'>$1</b>");
 	            }
 	            //恢复HTML标签
-	            for (var i = 0; i < arrA.length; i++) {
+	            for (let i = 0; i < arrA.length; i++) {
 	                temp = temp.replace("{[(" + i + ")]}", arrA[i]);
 	            }
 	            pucl.innerHTML = temp;
-	            //写入iframe
-	            /*
-	            var iframe = document.getElementById('iframe1');
-	            iframe.contentDocument.body.innerHTML = temp;
-	            */
+			},
+			searchHighLight2:function(idVal, keyword) {
+				var content = document.getElementById(idVal).innerHTML;
+				var s = keyword;
+			    var reg = new RegExp("(" + s + ")", "g");  
+			    var str = content;  
+			    var newstr = str.replace(reg, "<font color=red>$1</font>");
+				document.getElementById(idVal).innerHTML = newstr;
+
 			}
 		}
 	}
