98645078399ee148bf24aac809b55eee99555f11
programmer-zhang <chinajnzhang@hotmail.com>
2018-08-14 00:11:45 +0800

    提交jstest

--- src/views/jsTest.vue
+++ src/views/jsTest.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="js-test">
         <button @click="filterFalse()">filterFalse</button>
+        <button @click="addTen()">addTen</button>
     </div>
 </template>
 
@@ -17,8 +18,19 @@ export default {
     	filterFalse() {
     		let array = [3, 4, 5, 2, 3, undefined, null, 0, ""];
     		let returnData = array.filter(Boolean);
-    		return returnData;
     		console.log(returnData);
+    		return returnData;
+    	},
+    	addTen() {
+    		let users = [
+			    { username: "Kelly", isVIP: true, balance: 20 },
+			    { username: "Tom", isVIP: false, balance: 19 },
+			    { username: "Stephanie", isVIP: true, balance: 30 }
+			];
+			let returnData = users.map(
+			    user => (user.isVIP ? { ...user, balance: user.balance + 10 } : user)
+			);
+			console.log(returnData)
     	}
     }
 }
@@ -28,6 +40,7 @@ export default {
 <style lang="scss">
 .js-test {
     button {
+    	display: block;
     	width: auto;
     	height: 30px;
     	line-height: 30px;
