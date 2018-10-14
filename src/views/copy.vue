74368e74b74c9b01b5c673398526b88b32e57bdb
programmer-zhang <chinajnzhang@hotmail.com>
2018-10-14 22:53:01 +0800

    提交复制粘贴

--- src/views/copy.vue
+++ src/views/copy.vue
@@ -1,17 +1,65 @@
 <template>
-	<div class="copy">123</div>
+	<div class="copy">
+		<div class="copy-panel">
+			<span>请输入要粘贴的文字</span>
+			<input type="text" id="clipboard" v-model="copyData" placeholder="请输入文字">
+			<button @click="copy()">复制文字</button>
+		</div>
+		<div class="copy-panel">
+			<span>请点击鼠标右键或者Ctrl+V粘贴</span>
+			<input type="text" placeholder="请粘贴">
+		</div>
+	</div>
 </template>
 <script>
 	export default {
 		data:function() {
 			return {
-
+				copyData: '',
+			}
+		},
+		methods: {
+			copy:function() {
+				let data = '';
+				if(this.copyData) {
+					let spanSelect = document.querySelector('#clipboard');
+					spanSelect.select();
+					if(document.execCommand('copy')) {
+						document.execCommand('copy');
+						alert('复制成功');
+					} else {
+						alert('复制失败');
+					}
+				} else {
+					alert('复制失败');
+				}
 			}
 		}
 	}
 </script>
 <style lang="scss">
 .copy {
-	color: red;
+	.copy-panel {
+		margin: 10px 0px;
+		font-size: 14px;
+		span {
+			color: #333;
+			font-weight: bold;
+		}
+		input {
+			height: 30px;
+			width: 200px;
+			line-height: 30px;
+			border: 1px solid #dfdfdf;
+		}
+		button {
+			height: 30px;
+			width: 200px;
+			color: #fff;
+			font-weight: bold;
+			background-color: #ff556e;
+			border: none;
+		}
+	}
 }
 </style>
