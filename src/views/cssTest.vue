d368db0e0c244afe45650401b38ecbff9ce3994a
programmer-zhang <chinajnzhang@hotmail.com>
2018-10-22 22:49:35 +0800

    提交csstext

--- src/views/cssTest.vue
+++ src/views/cssTest.vue
@@ -1,5 +1,9 @@
 <template>
-	<div class="csstest">123</div>
+	<div class="csstest">
+		<div class="border-image">
+			<div class="border-content"></div>
+		</div>
+	</div>
 </template>
 <script>
 	export default {
@@ -11,13 +15,25 @@
 		mounted() {
 
 		},
-		methods() {
+		methods: {
 
 		}
 	}
 </script>
 <style lang="scss">
 .csstest {
-	
+	.border-image {
+		width: 100%;
+		border-bottom: 3px solid #000;
+		.border-content {
+			height: 100px;
+			width: 800px;
+			border: 30px solid transparent;
+			border-image: url(../../static/images/border.png)  30 30 round;
+			-moz-border-image:url(../../static/images/border.png) 30 30 round;	/* Old Firefox */
+			-webkit-border-image:url(../../static/images/border.png) 30 30 round;	/* Safari and Chrome */
+			-o-border-image:url(../../static/images/border.png) 30 30 round;		/* Opera */
+		}
+	}
 }
 </style>
