7bfa3693b3cbe12e117e2ffcdda67d7108ff4e07
programmer-zhang <chinajnzhang@hotmail.com>
2018-10-22 22:58:20 +0800

    完善csstest

--- src/views/cssTest.vue
+++ src/views/cssTest.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="csstest">
 		<div class="border-image">
-			<div class="border-content"></div>
+			<div class="border-content border-round"></div>
+			<div class="border-content border-stretch"></div>
 		</div>
 	</div>
 </template>
@@ -29,10 +30,18 @@
 			height: 100px;
 			width: 800px;
 			border: 30px solid transparent;
+		}
+		.border-round {
 			border-image: url(../../static/images/border.png)  30 30 round;
 			-moz-border-image:url(../../static/images/border.png) 30 30 round;	/* Old Firefox */
 			-webkit-border-image:url(../../static/images/border.png) 30 30 round;	/* Safari and Chrome */
 			-o-border-image:url(../../static/images/border.png) 30 30 round;		/* Opera */
+		}
+		.border-stretch {
+			border-image: url(../../static/images/border.png)  30 30 stretch;
+			-moz-border-image:url(../../static/images/border.png) 30 30 stretch;	/* Old Firefox */
+			-webkit-border-image:url(../../static/images/border.png) 30 30 stretch;	/* Safari and Chrome */
+			-o-border-image:url(../../static/images/border.png) 30 30 stretch;		/* Opera */
 		}
 	}
 }
