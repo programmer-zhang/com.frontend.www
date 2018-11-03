12f02a81c76cffafd57724ae7ad4888fb29d223c
programmer-zhang <chinajnzhang@hotmail.com>
2018-11-03 23:05:58 +0800

    提交helloworld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="hello">
         <div class="hello-item">
+            <a href="/home.html">Home</a>
         </div>
-        <a href="/home.html">Home</a>
         <a href="/highLight.html">HighLight</a>
         <a href="/pure.html">Pure</a>
         <a href="/pure-model.html">PureModel</a>
