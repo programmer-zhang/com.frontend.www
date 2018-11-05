0a833227f9283568d12a870a59e63e43fa871841
programmer-zhang <chinajnzhang@hotmail.com>
2018-11-05 22:14:37 +0800

    提交logopanel

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import countDown from '@/views/countDown.vue';
 import copy from '@/views/copy.vue';
 import changeDatas from '@/views/changeDatas.vue';
 import cssTest from '@/views/cssTest.vue';
+import wholeLife from '@/views/wholeLife.vue';
 
 Vue.use(Router)
 
@@ -59,6 +60,11 @@ export default new Router({
         path: '/cssTest.html',
         name: 'cssTest',
         component: cssTest
+    },
+    {
+        path: '/wholeLife.html',
+        name: 'wholeLife',
+        component: wholeLife
     }        
   ]
 })
---
+++ src/views/wholeLife.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="whole-life">
+		<div class="logo-panel">
+			<div class="left-side"></div>
+			<div class="logo-content"></div>
+			<div class="right-side"></div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+<style lang="scss">
+.whole-life {
+	background-color: black;
+	.logo-panel {
+		position: relative;
+		margin: 0 auto;
+		height: 600px;
+		width: 900px;
+		background-color: #fff;
+		.logo-content {
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			margin-top: -150px;
+			margin-left: -150px;
+			width: 300px;
+			height: 300px;
+			background-color: rgb(249,84,31);
+			border-radius: 50%;
+		}
+	}
+}
+</style>
