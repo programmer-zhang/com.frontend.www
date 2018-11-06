cf643da2232482068d0ab04887a0ca66b117041e
programmer-zhang <chinajnzhang@hotmail.com>
2018-11-06 17:15:56 +0800

    提交泰山

--- src/views/wholeLife.vue
+++ src/views/wholeLife.vue
@@ -1,9 +1,10 @@
 <template>
 	<div class="whole-life">
 		<div class="logo-panel">
-			<div class="left-side"></div>
-			<!-- <div class="logo-content"></div> -->
-			<div class="right-side"></div>
+			<div class="center-side">
+				<div class="item-panel" v-for="(item, index) in 3"></div>
+			</div>
+			<div class="logo-content"></div>
 		</div>
 	</div>
 </template>
@@ -26,22 +27,39 @@
 	.logo-panel {
 		position: relative;
 		margin: 0 auto;
-		height: 600px;
-		width: 900px;
-		background-color: #fff;
+		height: 540px;
+		width: 820px;
+		background-color: rgb(249,84,31);
 		.logo-content {
 			position: absolute;
 			left: 50%;
 			top: 50%;
 			margin-top: -150px;
-			margin-left: -150px;
-			width: 300px;
+			margin-left: -100px;
+			width: 200px;
 			height: 300px;
-			background-color: rgb(249,84,31);
-			border-radius: 50%;
+			background-color: #fff;
 		}
-		.left-side, .right-side {
-			
+		.center-side {
+			position: absolute;
+			left: -150px;
+			height: 210px;
+			width: 200%;
+			background-color: #000;
+			transform:rotate(-33deg);
+			.item-panel {
+				height: 70px;
+				background-color: #fff;
+				&:first-of-type {
+					background-color: #000;
+				}
+				&:nth-of-type(2n) {
+					background-color: rgb(249,84,31);
+				}				
+				&:last-of-type {
+					background-color: #fff;
+				}				
+			}
 		}
 	}
 }
