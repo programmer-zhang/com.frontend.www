053831b7e1687e22e23faaa20793b90b1a7d5706
programmer-zhang <chinajnzhang@hotmail.com>
2019-12-18 14:23:34 +0800

    updata extend

--- src/views/extend.vue
+++ src/views/extend.vue
@@ -24,6 +24,8 @@
     // 基本思想：利用原型让一个引用类型继承另外一个引用类型的属性和方法。
     // 构造函数，原型，实例之间的关系：
     // 每个构造函数都有一个原型对象，原型对象包含一个指向构造函数的指针，而实例都包含一个指向原型对象的内部指针
+    // 特点：基于原型链，既是父类的实例，也是子类的实例。
+    // 缺点：1.无法实现多继承；2.所有新实例都会共享父类实例的属性。
 
     class Animal {
         constructor(){
@@ -41,6 +43,8 @@
     
 
     // 构造函数继承
+    // 特点：可以实现多继承（call多个），解决了所有实例共享父类实例属性的问题。
+    // 缺点：1.只能继承父类实例的属性和方法；2.不能继承原型上的属性和方法。
     function SuperType() {
         this.colors = ["red", "blue", "green"];
     }
@@ -53,6 +57,43 @@
     console.log(instance1.colors); //"red,blue,green,black"
     let instance2 = new SubType();
     console.log(instance2.colors); //"red,blue,green" 
+
+    // 组合继承
+    // 特点：利用原型链继承父类的原型属性和方法，利用构造函数继承实例属性和方法
+    // 缺点：调用了两次父类构造函数，生成了两份实例
+    function SuperType(name) {
+        this.name = name;
+        this.colors = ["red", "blue", "green"];
+    }
+
+    SuperType.prototype.sayName = function() {
+        alert(this.name);
+    }
+
+    function SubType(name, age) {
+        // 继承属性
+        SuperType.call(this, name);
+        this.age = age;
+    }
+
+    // 继承方法
+    SubType.prototype = new SuperType();
+    SubType.prototype.constructor = SubType;
+    SubType.prototype.sayAge = function() {
+        alert(this.age);
+    };
+
+    let instance3 = new SubType("Nicholas", 29);
+    instance3.colors.push("black");
+    alert(instance3.colors); //"red,blue,green,black"
+    instance3.sayName(); //"Nicholas";
+    instance3.sayAge(); //29
+
+    let instance4 = new SubType("Greg", 27);
+    alert(instance4.colors); //"red,blue,green"
+    instance4.sayName(); //"Greg";
+    instance4.sayAge(); //27 
+
 
 </script>
 
