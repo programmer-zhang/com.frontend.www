7f6bbe25db6bc8428c87323b2341064f908d2395
programmer-zhang <chinajnzhang@hotmail.com>
2020-03-29 00:11:52 +0800

    updata extends

--- src/views/extend.vue
+++ src/views/extend.vue
@@ -25,70 +25,76 @@
     // animal.says('hi');  // animal says hi
 
     function SuperType(name) {
-        this.name = name || 'shit';
+        this.name = name;
         this.sexy = ["man", "woman", "unknow"];
         this.showName = function() {
             console.log(this.name);
         };
     };
-    // SuperType.prototype.age = 18;
+    SuperType.prototype.age = 18;
     // function SubType() {
     //     this.name = 'coder';
     // }
     // SubType.prototype = new SuperType();
-    // var subFun = new SubType();
-    // console.log('outer', subFun.age) // 18
-    // console.log('outer', subFun instanceof SuperType) // true 
+    // var instance = new SubType();
+    // console.log('outer', instance.age) // 18
+    // console.log('outer', instance instanceof SuperType) // true 
     
 
     // 构造函数继承
     // 特点：可以实现多继承（call多个），解决了所有实例共享父类实例属性的问题。
     // 缺点：1.只能继承父类实例的属性和方法；2.不能继承原型上的属性和方法。
-    function SubType() {
-        //继承SuperType
-        SuperType.call(this);
-    }
-    let instance1 = new SubType();
-    instance1.sexy.push("all");
-    console.log(instance1.sexy); //"["man", "woman", "unknow", "all"]"
-    let instance2 = new SubType();
-    console.log(instance2.sexy); //"["man", "woman", "unknow"]" 
+    // function SubType() {
+    //     //继承SuperType
+    //     SuperType.call(this);
+    // }
+    // let instance1 = new SubType();
+    // instance1.sexy.push("all");
+    // console.log(instance1.sexy); //"["man", "woman", "unknow", "all"]"
+    // let instance2 = new SubType();
+    // console.log(instance2.sexy); //"["man", "woman", "unknow"]" 
 
     // 组合继承
     // 特点：利用原型链继承父类的原型属性和方法，利用构造函数继承实例属性和方法
     // 缺点：调用了两次父类构造函数，生成了两份实例
-    // function SuperType(name) {
-    //     this.name = name;
-    //     this.colors = ["red", "blue", "green"];
-    // }
 
-    // SuperType.prototype.sayName = function() {
-    //     console.log(this.name);
-    // }
-
-    // function SubType(name, age) {
-    //     // 继承属性
+    // function SubType(name) {
     //     SuperType.call(this, name);
-    //     this.age = age;
+    // }
+    // SubType.prototype = new SuperType();
+    // var instance3 = new SubType("newCoder");
+    // console.log(instance3.name)
+
+    // ES6 extends继承
+    // class A {
+    //     constructor(name, age) {
+    //         this.name = name;
+    //         this.age = age;
+    //     }
+    //     getName() {
+    //         return this.name;
+    //     }
     // }
 
-    // // 继承方法
-    // SubType.prototype = new SuperType();
-    // SubType.prototype.constructor = SubType;
-    // SubType.prototype.sayAge = function() {
-    //     console.log(this.age);
-    // };
+    // class B extends A {
+    //     constructor(name, age) {
+    //         super(name, age);
+    //         this.job = "IT";
+    //     }
+    //     getJob() {
+    //         return this.job;
+    //     }
+    //     getNameAndJob() {
+    //         return super.getName() + this.job;
+    //     }
+    // }
 
-    // let instance3 = new SubType("Nicholas", 29);
-    // instance3.colors.push("black");
-    // console.log(instance3.colors); //"red,blue,green,black"
-    // instance3.sayName(); //"Nicholas";
-    // instance3.sayAge(); //29
-
-    // let instance4 = new SubType("Greg", 27);
-    // console.log(instance4.colors); //"red,blue,green"
-    // instance4.sayName(); //"Greg";
-    // instance4.sayAge(); //27 
+    // var b = new B("Tom", 20);
+    // console.log(b.name);
+    // console.log(b.age);
+    // console.log(b.getName());
+    // console.log(b.getJob());
+    // console.log(b.getNameAndJob());
 
     console.log('-------继承-------')
 </script>
