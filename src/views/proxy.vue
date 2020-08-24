5d464f89184a7d25dfb6c6b14fbc2a0c864d06db
programmer-zhang <chinajnzhang@hotmail.com>
2020-08-25 00:08:58 +0800

    updata proxy

--- src/views/proxy.vue
+++ src/views/proxy.vue
@@ -112,6 +112,7 @@
     /**
     * 二：利用 proxy 进行数据校验
     * 3. 如果要直接为对象的所有属性开发一个校验器可能很快就会让代码结构变得臃肿，使用 Proxy 则可以将校验器从核心逻辑分离出来自成一体.
+    * 升级版这里有问题
     */
     // function createValidator(target, validator) {
     //     return new Proxy(target, {
@@ -120,14 +121,16 @@
     //             if (target.hasOwnProperty(prop)) {
     //                 let validator = this._validator[prop];
     //                 if (!!validator(value)) {
-    //                     return Reflect.set(target, prop, value);
+    //                     // return Reflect.set(target, prop, value);
     //                 }
     //                 else {
-    //                     throw Error(`Cannot set ${prop} to ${value}. Invalid.`);
+    //                     console.log(`Cannot set ${prop} to ${value}. Invalid.`);
+    //                     // throw Error(`Cannot set ${prop} to ${value}. Invalid.`);
     //                 }
     //             }
     //             else {
-    //                 throw Error(`${prop} is not a valid property`);
+    //                 console.log(`${prop} is not a valid property`);
+    //                 // throw Error(`${prop} is not a valid property`);
     //             }
     //         }
     //     });
@@ -151,9 +154,58 @@
     // }
 
     // const bill = new Person('Bill', 25); // 以下操作都会报错
-    // bill.name = 0;
+    // // bill.name = 0;
     // bill.age = 'Bill';
     // bill.age = 15;
+
+    /**
+    * 三: 利用proxy进行mock数据
+    * 1. 开发过程中经常需要mock数据，
+    */
+
+    /**
+    * 四: Proxy & Object.defineProperty
+    * 1. Object.defineProperty 无法一次性监听对象所有属性，必须遍历或者递归来实现
+    * 2. Object.defineProperty 无法监听新增加的属性
+    */
+    // let boy = {
+    //     name: 'Jackson',
+    //     age: 22
+    // }
+    // /* Proxy 监听整个对象*/
+    // boy = new Proxy(boy, {
+    //     set: function(target, prop, value) {
+    //         target[prop] = value + '&Rose';
+    //         return true;
+    //     },
+    //     get: function(target, prop) {
+    //         return target[prop] + '&Proxy';
+    //     }
+    // })
+    // boy.name = 'Jack';
+    // boy.sex = 'male'
+    // console.log(boy.sex);
+
+
+    /* Object.defineProperty */
+    // let girl = {
+    //     name: 'Rose',
+    //     age: 22
+    // }
+    // Object.keys(girl).forEach(key => {
+    //     console.log(key)
+    //     Object.defineProperty(girl, key, {
+    //         set: function(value) {
+    //             girl[key] = value;
+    //         },
+    //         get: function(target, key) {
+    //             return girl[key] + '&defineProperty';
+    //         }
+    //     })
+    // })
+
+    // girl.sex = 'female';
+    // console.log(girl.sex);
 
 </script>
 
